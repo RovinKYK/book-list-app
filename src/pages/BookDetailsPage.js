e828f540ed24ca082fd71da6b99527a91b1da879
RovinKYK <kykrovin@gmail.com>
2025-09-01 17:32:39 +0530

    deleted all

--- src/pages/BookDetailsPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BookDetail from '../components/BookDetail';
-
-const BookDetailsPage = () => {
-  return <BookDetail />;
-};
-
-export default BookDetailsPage;
--- src/pages/EditBookPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BookForm from '../components/BookForm';
-
-const EditBookPage = () => {
-  return <BookForm isEditing={true} />;
-};
-
-export default EditBookPage;

9a10220b6e67260486bc2a750502d9215346d424
RovinKYK <kykrovin@gmail.com>
2025-09-02 18:35:48 +0530

    clear repo

--- public/config.js
+++
@@ -1,6 +0,0 @@
-// This file will be dynamically created by Choreo during deployment
-// It will contain the configurations needed to connect to the backend API
-window.configs = window.configs || {
-  // Default configuration for local development
-  apiUrl: 'http://localhost:8080'
-};

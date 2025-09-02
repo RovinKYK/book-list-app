5b2f115101447fd2b5b5fa95e327a8d5dc924bd5
RovinKYK <kykrovin@gmail.com>
2025-09-02 14:42:55 +0530

    clear repo

--- public/config.js
+++
@@ -1,5 +0,0 @@
-// This file will be replaced by Choreo during deployment
-// with the appropriate configuration values
-window.configs = {
-  apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:8080'
-};

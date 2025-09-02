d54e0dd05fdd3b345577666d4063f6890f8c914a
RovinKYK <kykrovin@gmail.com>
2025-09-02 14:56:10 +0530

    clear repo

--- public/config.js
+++
@@ -1,5 +0,0 @@
-window.configs = {
-  // This file will be overwritten by Choreo when deployed in a Choreo environment
-  // For local development, this file serves as a placeholder
-  apiUrl: '/choreo-apis/book-list-service'
-};
